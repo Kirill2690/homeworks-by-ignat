87291c415dc917786ecdca230dfdb1a6db23dc6f
Kirill2690 <kirillmuravev2690@gmail.com>
2022-06-26 11:13:41 +0300

    Updates

---
+++ precache-manifest.080c73e5a245c6abce00f695a07035d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5cfd7dcb10b0a1b6d1d6914f5b011a4",
+    "url": "/Kirill2690/homeworks-by-ignat/index.html"
+  },
+  {
+    "revision": "f9c257de7d33bbcc1742",
+    "url": "/Kirill2690/homeworks-by-ignat/static/css/main.1776dcad.chunk.css"
+  },
+  {
+    "revision": "7850fad9c3c5b4dff7a7",
+    "url": "/Kirill2690/homeworks-by-ignat/static/js/2.bd4614ab.chunk.js"
+  },
+  {
+    "revision": "248fe9facfff898840212404b4cb88d9",
+    "url": "/Kirill2690/homeworks-by-ignat/static/js/2.bd4614ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9c257de7d33bbcc1742",
+    "url": "/Kirill2690/homeworks-by-ignat/static/js/main.b992bf32.chunk.js"
+  },
+  {
+    "revision": "a46b383c66ca7b6ed65f",
+    "url": "/Kirill2690/homeworks-by-ignat/static/js/runtime-main.d91184f3.js"
+  }
+]);
