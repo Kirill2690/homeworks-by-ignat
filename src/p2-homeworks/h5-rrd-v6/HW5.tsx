224c3e92549ee17d2cadf7c5e1a8870baeba950e
Kirill2690 <kirillmuravev2690@gmail.com>
2022-06-26 10:25:02 +0300

    add commit

--- src/p2-homeworks/h5-rrd-v6/HW5.tsx
+++ src/p2-homeworks/h5-rrd-v6/HW5.tsx
@@ -3,7 +3,7 @@ import Header from './Header'
 import Pages from './Pages'
 import {HashRouter} from "react-router-dom";
 import Error404 from "../h2/tests/h5-rrd-v6/pages/Error404";
-
+///
 function HW5() {
     return (
         <div>
