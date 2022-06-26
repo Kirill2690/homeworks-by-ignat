0654f73a5a167d3a506764e8a817e99b700d5933
Kirill2690 <kirillmuravev2690@gmail.com>
2022-06-26 11:37:15 +0300

    add commit

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -3,7 +3,7 @@ import {NavLink} from "react-router-dom";
 import {PATH} from "./Pages";
 import s from './Header.module.css'
 
-
+///
 function Header() {
     return (
         <div className={s.header}>
